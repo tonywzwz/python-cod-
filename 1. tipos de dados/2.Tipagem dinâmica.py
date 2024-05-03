0aeec42154d615179768a50ad418d0f23403819d
tonywzwz <161043062+tonywzwz@users.noreply.github.com>
2024-05-03 12:24:16 +0000

    1º python

---
+++ 1. tipos de dados/2.Tipagem dinâmica.py
@@ -0,0 +1,11 @@
+# tipagem dinâmica.
+
+#atribuindo valores
+nome = "Marta"
+idade = 20
+peso = 62.500
+
+#Exibindo dados.
+print(f"Nome : {nome}")
+print(f"idade : {idade}")
+print(f"peso : {peso}")
---
+++ 1. tipos de dados/Óla mundo.py
@@ -0,0 +1,9 @@
+# Isso é um comentario de uma linha.
+
+'''
+    Isso é um comentario
+           de
+           múltiplas linhas.
+'''
+
+print("Olá mundo!")
